72a12cfa9457152b27c0f507548495bbcd3e32e8
Evan Ryan <evan.hundred@gmail.com>
2022-11-07 16:55:28 -0500

    build movingObject draw

--- asteroids/src/index.js
+++ asteroids/src/index.js
@@ -1,3 +1,33 @@
 console.log("Webpackin'");
 
 const MovingObject = require("./moving_object");
+window.MovingObject = MovingObject;
+
+const canvasEl = document.getElementById("canvas");
+canvasEl.width = 500;
+canvasEl.height = 500;
+
+const ctx = canvasEl.getContent("2d");
+
+const mo = new MovingObject({
+  pos: [30, 30],
+  vel: [10, 10],
+  radius: 5,
+  color: "#00FF00",
+});
+
+// mo.draw(ctx);
+
+// create moving object instance
+// invoke draw method - pass ctx as argument
+
+// ctx.fillRect(0,0,500,500);
+
+// ctx.beginPath();
+// ctx.strokeStyle = "white";
+// ctx.lineWidth = 30; // pixels
+// ctx.arc(250,250,80,0,2*Math.PI,true;);
+//   begin in center of canvas, r = 80;
+// ctx.stroke();
+// ctx.fillStyle = "blue";
+// ctx.fill();
--- asteroids/src/moving_object.js
+++ asteroids/src/moving_object.js
@@ -1,10 +1,17 @@
 function MovingObject(options) {
-  this.pos = options[pos];
-  this.vel = options[vel];
-  this.radius = options[radius];
-  this.color = options[color];
+  this.pos = options.pos;
+  this.vel = options.vel;
+  this.radius = options.radius;
+  this.color = options.color;
 }
 
 module.exports = MovingObject;
 
-MovingObject.prototype.draw = function (ctx) {};
+MovingObject.prototype.draw = function (ctx) {
+  ctx.fillStyle = "blue";
+  ctx.beginPath();
+  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+  // take data from movingobject invoked upon
+  // make drawing on ctx
+};
